9bcbbe082bebfdea8d284dd272d36d282d969763
Chris-Delfaus <ChrisDelf@protonmail.com>
2019-08-07 16:02:03 -0400

    working on part four

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -6,9 +6,7 @@ import FormikUserForm from './Components/userForm';
 function App() {
   return (
     <div className="App">
-              <p>
-          Edit <code>src/App.js</code> and save to reload.
-    </p>
+
     <FormikUserForm/>
        </div>
   );
--- user-onboarding/src/Components/userForm.js
+++ user-onboarding/src/Components/userForm.js
@@ -3,19 +3,30 @@ import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = ({errors, touched, values}) => {
+const UserForm = ({ errors, touched, values }) => {
+
   return (
     <div>
       <h2>New User Form</h2>
       <Form>
         <Field type="text" name="name" placeholder="Name..." />
+        {touched.name && errors.name && <p className="error">{errors.name}</p>}
         <Field type="text" name="email" placeholder="email..." />
-        <Field type="text" name="password" placeholder="password.." />
+        {touched.email && errors.email && (
+          <p className="error">{errors.email}</p>
+        )}
+        <Field type="password" name="password" placeholder="password.." />
+        {touched.password && errors.password && (
+          <p className="error">{errors.password}</p>
+        )}
         <label className="checkbox-container">
           Do you accept the terms of service?
-          <Field type="checkbox" name="terms" checked={values.terms} />
+    <Field type="checkbox" name="terms" checked={values.terms} />
+    {errors.terms && <p className="error">{errors.terms}</p>}
           <span className="checkmark" />
         </label>
+
+
         <button type="submit">Create Account</button>
       </Form>
     </div>
@@ -29,7 +40,7 @@ const FormikUserForm = withFormik({
       name: name || '',
       email: email || '',
       password: password || '',
-      terms: terms || false
+      terms: terms || false,
     };
   },
   //=== ValidationSchema nice tool to inculde error messages
@@ -38,14 +49,17 @@ const FormikUserForm = withFormik({
     name: Yup.string().required('Please enter a name'),
     email: Yup.string().required('Enter an email'),
     password: Yup.string().required('Create a password'),
-    terms: Yup.boolean().required('Please accept the terms of serivce'),
+    terms: Yup.bool().oneOf([true], 'Please accept the terms of serivce'),
   }),
- handleSubmit(values) {
+  handleSubmit(values, {resetForm}) {
     axios
       .post('https://reqres.in/api/users/', values)
-      .then(res => console.log(res))
+      .then(res => {console.log(res)
+        resetForm();
+      })
       .catch(err => console.log(err.response));
   }
+
 })(UserForm);
 
 export default FormikUserForm;
